315490e699115c07ae1d0b4d0c46a70b8e94d0fd
Dragos Roua <dragosroua@gmail.com>
2017-10-31 09:23:02 -0400

    boilerplate added

---
+++ includes/class-presteem-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://dragosroua.com
+ * @since      1.0.0
+ *
+ * @package    Presteem
+ * @subpackage Presteem/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Presteem
+ * @subpackage Presteem/includes
+ * @author     Dragos Roua <dragosroua@gmail.com>
+ */
+class Presteem_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
